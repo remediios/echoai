d7a4627f672cd0bc1e7603997234048cf3336512
Miguel Remédios <miguelremediios@gmail.com>
2024-04-19 00:30:41 +0100

    feat: configure tailwindcss

--- app/index.jsx
+++ app/index.jsx
@@ -4,21 +4,10 @@ import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>EchoAI</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">EchoAI</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue' }}>
-        Go to profile
-      </Link>
+      <Link href="/profile">Go to profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
