a2bcef99db06803099980452f3200c33e4343b02
Miguel Remédios <miguelremediios@gmail.com>
2024-04-19 00:54:55 +0100

    chore: add assets and constants

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
       colors: {
         primary: '#161622',
         secondary: {
-          DEFAULT: '#FF9C01',
-          100: '#FF9001',
-          200: '#FF8E01',
+          DEFAULT: '#910101',
+          100: '#de2121',
+          200: '#c21d1d',
         },
         black: {
           DEFAULT: '#000',
