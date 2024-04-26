a2c5078558ae3ee6816f1d86d458217de012c3ca
Miguel Remédios <miguelremediios@gmail.com>
2024-04-26 20:25:46 +0100

    feat: add video player from cloud database videos

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import {
-  View,
-  Text,
   FlatList,
   TouchableOpacity,
   ImageBackground,
   Image,
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import { Video, ResizeMode } from 'expo-av';
+
 import { icons } from '../constants';
 
 const zoomIn = {
@@ -30,6 +30,7 @@ const zoomOut = {
 
 const TrendingItem = ({ activeItem, item }) => {
   const [play, setPlay] = useState(false);
+
   return (
     <Animatable.View
       className="mr-5"
@@ -37,18 +38,32 @@ const TrendingItem = ({ activeItem, item }) => {
       duration={500}
     >
       {play ? (
-        <Text className="text-white">Playing</Text>
+        <Video
+          source={{ uri: item.video }}
+          className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
       ) : (
         <TouchableOpacity
-          className="relative justify-center items-center"
+          className="relative flex justify-center items-center"
           activeOpacity={0.7}
           onPress={() => setPlay(true)}
         >
           <ImageBackground
-            source={{ uri: item.thumbnail }}
-            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black-40"
+            source={{
+              uri: item.thumbnail,
+            }}
+            className="w-52 h-72 rounded-[33px] my-5 overflow-hidden shadow-lg shadow-black/40"
             resizeMode="cover"
           />
+
           <Image
             source={icons.play}
             className="w-12 h-12 absolute"
